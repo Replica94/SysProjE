e9ceaa3055673ebd87c0e7c69d8bdb60ad17f751
EppuHeilimo <eppu.heilimo@yahoo.com>
2016-02-22 13:53:06 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-var express  = require('express');
-
-var app = express();
-require('./routes')(app);
-app.use(express.static(__dirname + '/public'));
-app.listen(process.env.PORT || 8080);
-
-exports = module.exports = app;
