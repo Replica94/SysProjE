04d23db60094e783cc7b40e100649b9f2d26b600
MasterTaffer <odestrzo@gmail.com>
2016-02-24 14:03:55 +0200

    Add some comments

--- public/js/object/gameObject.js
+++ public/js/object/gameObject.js
@@ -1,19 +1,38 @@
+
+
+
 function GameObject()
 {
+	//Bitwise OR of all wanted contexts, or zero for all 
 	this.drawContext = 0;
+	//Bitwise OR of all wanted contexts, or zero for all
 	this.inputContext = 0;
+	//The z index; greater z is "closer" to the screen
 	this.z = 0;
+	//Is set by the Engine, if the mouse is hovering on the object
 	this.mouseHover = false;
+	//Whether the Engine should send input events to this object
 	this.checkForInput = false;
+	//The object bounds for input checking
 	this.bounds = new AABB(0, 0);
+	//The method used to draw the object
 	this.draw = null;
+	//The method used for updating the object
 	this.update = null;
+	//The onClick callback
 	this.onClick = null;
+	//If the following is true, object will be deleted at next step
 	this.isDoomed = true;
 }
 
-TestObject.prototype = new GameObject();
-TestObject.constructor = new TestObject();
+//How to inherit stuff in JavaScript?
+//
+//var TestObject = function(){stuff...}
+//
+//TestObject.prototype = new GameObject();
+//TestObject.constructor = new TestObject();
+
+//Call addObject for stuff
 
 var Engine =
 {
@@ -47,7 +66,7 @@ var Engine =
 	{
 		
 	},
-	draw: function ()
+	draw: function () //TODO
 	{
 		
 	}
