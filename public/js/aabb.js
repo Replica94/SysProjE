c2bd5e9a57e603e2bafe9e89bcb18bec5e3286fd
MasterTaffer <odestrzo@gmail.com>
2016-02-26 11:16:13 +0200

    Add JSDoc fun stuff [ci skip]
    See public/js/aabb.js for JSDoc documentation example.

--- public/js/aabb.js
+++ public/js/aabb.js
@@ -1,10 +1,16 @@
+/**
+ 	Constructor for Axis Aligned Bounding Box struct
+	@param {Vector2} (aa) top-left of the bounding box
+	@param {Vector2} (bb) bottom-right of the bounding box
+	@constructor
+*/
 function AABB(aa, bb)
 {
 	this.aa = aa.copy();
 	this.bb = bb.copy();
 }
 
-//Generates an AABB from size and position (top-left)
+/** Generates an AABB from size and position (top-left) */
 function GenerateAABB(position, size)
 {
 	return new AABB(position,position.add(size));
