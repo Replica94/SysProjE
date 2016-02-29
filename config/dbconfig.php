620698d49ce53f307905c95ade0632f900ecb59c
karelian <karelianus@gmail.com>
2016-02-29 02:24:00 +0100

    Add database config

---
+++ config/dbconfig.php
@@ -0,0 +1,14 @@
+﻿<?php
+// Application database
+$appdb['host'] = getenv('DATABASE_HOST');
+$appdb['port'] = getenv('DATABASE_PORT');
+$appdb['username'] = getenv('DATABASE_USERNAME');
+$appdb['password'] = getenv('DATABASE_PASSWORD');
+$appdb['database'] = getenv('DATABASE_DATABASE');
+
+// Medical database
+$drugdb['host'] = getenv('DRUGDB_HOST');
+$drugdb['port'] = getenv('DRUGDB_PORT');
+$drugdb['username'] = getenv('DRUGDB_USERNAME');
+$drugdb['password'] = getenv('DRUGDB_PASSWORD');
+$drugdb['database'] = getenv('DRUGDB_DATABASE');
