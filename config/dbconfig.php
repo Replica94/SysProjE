6afd5fab86433d29c32a159c3fdaa7f40b01255f
karelian <karelianus@gmail.com>
2016-03-03 22:18:19 +0200

    Fix missing parenthesis

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -2,7 +2,7 @@
 // Application database
 define('APPDB_HOST', getenv('DATABASE_HOST'));
 define('APPDB_PORT', getenv('DATABASE_PORT'));
-define('APPDB_USERNAME', getenv('DATABASE_USERNAME');
+define('APPDB_USERNAME', getenv('DATABASE_USERNAME'));
 define('APPDB_PASSWORD', getenv('DATABASE_PASSWORD'));
 define('APPDB_DATABASE', getenv('DATABASE_DATABASE'));
 
