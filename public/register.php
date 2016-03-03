3c0b6bd9715fe134d4ba8d9f359283132befac53
karelian <karelianus@gmail.com>
2016-03-03 21:45:46 +0100

    Move HTML to template files

--- public/register.php
+++ public/register.php
@@ -60,23 +60,6 @@ if (isset($_POST['submit'])) {
 		}
 	}
 }
-?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Register</title>
-	</head>
 
-	<body>
-		<form method="post" action="register.php">
-			<input type="text" name="username" placeholder="Type a username">
-			<input type="password" name="passwd" placeholder="Type a password">
-			<input type="password" name="passwd2" placeholder="Retype password">
-			<input type="submit" name="submit" value="Register">
-		</form>
-		<?php foreach ($warnings as $warning): ?>
-			<div class="warning"><?php echo $warning ?></div>
-		<?php endforeach; ?>
-	</body>
-</html>
+// Include the HTML template:
+require __DIR__ . '/../templates/register.php';
---
+++ templates/login.php
@@ -0,0 +1,21 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Login</title>
+	</head>
+
+	<body>
+		<?php foreach ($messages as $message): ?>
+			<div class="message"><?php echo $message ?></div>
+		<?php endforeach; ?>
+		<form method="post" action="login.php">
+			<input type="text" name="username" placeholder="Type a username">
+			<input type="password" name="passwd" placeholder="Type a password">
+			<input type="submit" name="submit" value="Register">
+		</form>
+		<?php foreach ($warnings as $warning): ?>
+			<div class="warning"><?php echo $warning ?></div>
+		<?php endforeach; ?>
+	</body>
+</html>
