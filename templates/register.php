a9b31b89d25e68b484ec10022bef58559a48aab5
Replica94 <santeritaponen@gmail.com>
2016-03-07 16:00:26 +0200

    Add style to register

--- templates/register.php
+++ templates/register.php
@@ -3,17 +3,20 @@
     <head>
         <meta charset="UTF-8">
         <title>Register</title>
-        
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
         <!-- reCAPTCHA: -->
         <script src='https://www.google.com/recaptcha/api.js'></script>
     </head>
 
     <body>
         <form method="post" action="register.php">
-            <input type="text" name="username" placeholder="Type a username">
-            <input type="password" name="passwd" placeholder="Type a password">
-            <input type="password" name="passwd2" placeholder="Retype password">
-            <input type="submit" name="submit" value="Register">
+			<label class="w3-label" for="username">Username</label>
+            <input class="w3-input" type="text" name="username" placeholder="Type a username">
+			<label class="w3-label" for="passwd">Password</label>
+            <input class="w3-input" type="password" name="passwd" placeholder="Type a password">
+			<label class="w3-label" for="passwd2">Repeat password</label>
+            <input class="w3-input" type="password" name="passwd2" placeholder="Retype password">
+            <input class="w3-input" type="submit" name="submit" value="Register">
             
             <!-- reCAPTCHA: -->
             <div class="g-recaptcha" data-sitekey="6Lf48xkTAAAAAGjaAyRttKbbbc5vD3X4i3iUEwg9"></div>
