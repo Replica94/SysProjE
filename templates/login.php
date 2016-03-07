97c4bc7aa717ac8fc62d5ffe6bda9e2bb6af5e33
LauriKorte <H8805@student.jamk.fi>
2016-03-07 15:08:59 +0200

    Do some login and css style additioness

--- templates/login.php
+++ templates/login.php
@@ -3,20 +3,27 @@
     <head>
         <meta charset="UTF-8">
         <title>Login</title>
+		<link rel="stylesheet" type="text/css" href="styles.css">
+			
     </head>
 
     <body>
+	<section>
         <?php foreach ($messages as $message): ?>
             <div class="message"><?php echo $message ?></div>
         <?php endforeach; ?>
         <form method="post" action="login.php">
-            <input type="text" name="username" placeholder="Type a username">
-            <input type="password" name="passwd" placeholder="Type a password">
-            <input type="checkbox" name="remember_me" value="true">
-            <input type="submit" name="submit" value="Register">
+				<label class="w3-label" for="username">Username</label>
+				<input class="w3-input" type="text" name="username" placeholder="Type a username"><p>
+				<label for="passwd">Password</label>
+				<input class="w3-input" type="password" name="passwd" placeholder="Type a password"><p>
+				<input class="w3-check" type="checkbox" name="remember_me" value="true">
+				<label for="checkbox">Remember me</label><p>
+				<input class="w3-input" type="submit" name="submit" value="Login">
         </form>
         <?php foreach ($warnings as $warning): ?>
             <div class="warning"><?php echo $warning ?></div>
         <?php endforeach; ?>
+	</section>
     </body>
 </html>
