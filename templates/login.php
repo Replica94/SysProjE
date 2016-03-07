2ea2c70e29c6aa08e7269ece06b1621bfdbd9855
LauriKorte <lauri.korte@hotmail.com>
2016-03-07 15:18:43 +0200

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Login</title>
-		<link rel="stylesheet" type="text/css" href="styles.css">
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
 			
     </head>
 <!-- this is useless comment -->
