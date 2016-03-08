b30d52c3fda42591c191c983710507e6ef5cc90c
karelian <karelianus@gmail.com>
2016-03-08 08:33:45 +0100

    Fix HTML validation errors

--- templates/login.php
+++ templates/login.php
@@ -13,11 +13,11 @@
         <?php endforeach; ?>
         <form method="post" action="login.php">
 				<label class="w3-label" for="username">Username</label>
-				<input class="w3-input" type="text" name="username" placeholder="Type a username"><p>
+				<input class="w3-input" type="text" id="username" name="username" placeholder="Type a username"><p>
 				<label class="w3-label" for="passwd">Password</label>
-				<input class="w3-input" type="password" name="passwd" placeholder="Type a password"><p>
-				<input class="w3-check" type="checkbox" name="remember_me" value="true">
-				<label for="checkbox">Remember me</label><p>
+				<input class="w3-input" type="password" id="passwd" name="passwd" placeholder="Type a password"><p>
+				<input class="w3-check" type="checkbox" id="remember_me" name="remember_me" value="true">
+				<label for="remember_me">Remember me</label><p>
 				<input class="w3-input" type="submit" name="submit" value="Login">
         </form>
         <?php foreach ($warnings as $warning): ?>
