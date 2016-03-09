24dc134014390abfa611deaf0ee5c6a716f094fe
MasterTaffer <odestrzo@gmail.com>
2016-03-09 13:04:46 +0200

    Add base menu functionality

--- public/js/object/context.js
+++ public/js/object/context.js
@@ -1,8 +1,10 @@
 var Context = {
 	map : 
 	{
-		"nullContext":		0,
-		"mainMenu":			1,
-		"gameScreenDesk":	2
+		"nullContext":				0,
+		"mainMenu":					1,
+		"gameScreenDesk":			2,
+		"gameScreenSomethingElse":	3,
+		"menus":					[1], //List of all menus
 	}
 };
