167488bf873051a77965263d9b17f208f7312ede
EppuHeilimo <eppu.heilimo@yahoo.com>
2016-03-15 13:22:33 +0200

    Update person targetpos
    # Conflicts:
    #	public/js/canvas.js
    #	public/js/person.js

--- public/js/person.js
+++ public/js/person.js
@@ -140,7 +140,7 @@ function Person()
 	this.sizebody = new Vector2(200, 300);
 	this.drawOffset = Context.drawOffset["behindDesk"];
     this.depth = -30;
-	this.targetpos = new Vector2(1000 + Persons.allPersons.length * 100, -100);
+	this.targetpos = new Vector2(screenSize.x/2 + Persons.allPersons.length * 100, -100);
 	this.entering = true;
     this.a = 0;
     this.kasvaako = false;
