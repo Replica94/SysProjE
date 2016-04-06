bd173e5e34ab56cb38c19984be7f4a531b736abe
karelian <karelianus@gmail.com>
2016-04-06 13:29:17 +0200

    Add other difficulties to highscore list

--- public/highscore.php
+++ public/highscore.php
@@ -5,6 +5,8 @@ autoloader(__DIR__ . "/../lib/");
 autoloader(__DIR__ . "/../lib/dao/");
 
 $scores["easy"] = User::getHallOfFame(1, 10);
-
+$scores["medium"] = User::getHallOfFame(2, 10);
+$scores["hard"] = User::getHallOfFame(3, 10);
+$scores["lunatic"] = User::getHallOfFame(4, 10);
 
 include("../templates/highscore.php");
