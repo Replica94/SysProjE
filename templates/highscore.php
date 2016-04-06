5aaba0e4968995ab132111b52acf9734c13f55ee
karelian <karelianus@gmail.com>
2016-04-06 12:53:41 +0200

    Style highscore

--- templates/highscore.php
+++ templates/highscore.php
@@ -3,16 +3,21 @@
     <head>
         <meta charset="UTF-8">
         <title>Highscore</title>
+        <link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
     </head>
-    <body>
+    <body style= "font-family: Inconsolata">
 
         <?php foreach ($scores as $difficulty => $scorelist): ?>
             <h1><?php echo $difficulty; ?></h1>
-            <ol>
+            <table>
                 <?php foreach ($scorelist as $rank => $score): ?>
-                    <li><?php echo $score["username"] . " " . $score["highscore"]; ?></li>
+                    <tr>
+                        <td><?php echo '#' . ($rank + 1); ?></td>
+                        <td><?php echo $score["username"]; ?></td>
+                        <td><?php echo $score["highscore"]; ?></td>
+                    </tr>
                 <?php endforeach; ?>
-            </ol>
+            </table>
         <?php endforeach; ?>
     
 	</body>
